41b5aa4c09d11acad36e19df205d04ef643ea593
Alex Campayo <alex.campayo@gmail.com>
2019-05-14 21:41:02 +0200

    Project creation

---
+++ expo-toys-ios/expo-toys-iosUITests/expo_toys_iosUITests.swift
@@ -0,0 +1,34 @@
+//
+//  expo_toys_iosUITests.swift
+//  expo-toys-iosUITests
+//
+//  Created by Alex Campayo on 14/05/2019.
+//  Copyright © 2019 Alex Campayo. All rights reserved.
+//
+
+import XCTest
+
+class expo_toys_iosUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
